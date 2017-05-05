4e4cb8b296fbe79bc0a1d1b48b5b464e1eae318a
alojua <juan.alonso@staempfli.com>
2017-05-05 23:54:12 +0200

    Add dummy example of Jenkins shared-library and Readme.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jenkins basic shared library sample
+
+Jenkins shared library example that can be imported by other projects inside the Jenkinsfile.
+
+This repository has was created to provide an example for a tutorial about how to set up Jenkins Pipelines:
+
+* [Setup CI/CD with Jenkins, pipelines and Blueocean in 4 Steps](#)
---
+++ vars/standardPipeline.groovy
@@ -0,0 +1,40 @@
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    node {
+	    // Clean workspace before doing anything
+	    deleteDir()
+
+	    try {
+	        stage ('Clone') {
+	        	checkout scm
+	        }
+	        stage ('Build') {
+	        	sh "echo 'building ${config.projectName} ...'"
+	        }
+	        stage ('Tests') {
+		        parallel 'static': {
+		            sh "echo 'shell scripts to run static tests...'"
+		        },
+		        'unit': {
+		            sh "echo 'shell scripts to run unit tests...'"
+		        },
+		        'integration': {
+		            sh "echo 'shell scripts to run integration tests...'"
+		        }
+	        }
+	      	stage ('Deploy') {
+	            sh "echo 'deploying to server ${config.serverDomain}...'"
+	      	}
+	    } catch (err) {
+	        currentBuild.result = 'FAILED'
+	        throw err
+	    }
+    }
+}
+
+
+
